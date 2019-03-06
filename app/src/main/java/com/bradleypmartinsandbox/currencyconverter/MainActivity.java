a4678e05af68f2dd9ab0ffaf1b95caffc4ff07d3
Brad Martin <brma7253@colorado.edu>
2019-03-05 21:56:10 -0700

    finishing up converter project for now

--- app/src/main/java/com/bradleypmartinsandbox/currencyconverter/MainActivity.java
+++ app/src/main/java/com/bradleypmartinsandbox/currencyconverter/MainActivity.java
@@ -5,13 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     public void convertCurrency(View view) {
-        Log.i("Info","Button pressed.");
+        Log.i("CurrencyDebug","Button pressed.");
 
         EditText editText = findViewById(R.id.editText);
+
+        Log.i("CurrencyDebug", "editText text: " + editText.getText().toString());
+
+        String amountInPounds = editText.getText().toString();
+
+        double amtInPoundsDouble = Double.parseDouble(amountInPounds);
+        double amtInDollarsDouble = amtInPoundsDouble * 1.3;
+
+        String amountInDollarsStr = String.format(Locale.ENGLISH, "%.2f", amtInDollarsDouble);
+
+        Log.i("CurrencyDebug", "Converted dollars (str): " + amountInDollarsStr);
+
+        Toast.makeText(this, "You have $" + amountInDollarsStr + " worth of GBP.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
