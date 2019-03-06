a8882c6b27131510e94b88b56224057b9f29d7a9
Brad Martin <brma7253@colorado.edu>
2019-03-05 21:38:54 -0700

    layout set; configuring convert button

--- app/src/main/java/com/bradleypmartinsandbox/currencyconverter/MainActivity.java
+++ app/src/main/java/com/bradleypmartinsandbox/currencyconverter/MainActivity.java
@@ -2,8 +2,17 @@ package com.bradleypmartinsandbox.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        Log.i("Info","Button pressed.");
+
+        EditText editText = findViewById(R.id.editText);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
